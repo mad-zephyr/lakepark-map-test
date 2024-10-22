7355671e6f300b01b224f88b5c702cfb1d15a382
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2024-10-22 07:04:58 +0300

    update unused imports

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 // https://tmpfiles.org/dl/14638176/house_2.glb
 import Map, {
-  AttributionControl,
   FullscreenControl,
   GeolocateControl,
-  Marker,
   NavigationControl,
-  Popup,
   ScaleControl,
+  ViewStateChangeEvent,
 } from "react-map-gl";
 import { FC, useState } from "react";
 
@@ -15,25 +13,25 @@ import classes from "./style.module.sass";
 
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN || "";
 
-type TPosition = {
-  longitude: number;
-  latitude: number;
-  city: string;
-  state: string;
-  image: string;
-};
+// type TPosition = {
+//   longitude: number;
+//   latitude: number;
+//   city: string;
+//   state: string;
+//   image: string;
+// };
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Page: FC = () => {
-  const [popupInfo, setPopupInfo] = useState<TPosition | null>({
-    longitude: 28.691597768997394,
-    latitude: 47.10890434472199,
-    city: "",
-    state: "",
-    image:
-      "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
-  });
+  // const [popupInfo, setPopupInfo] = useState<TPosition | null>({
+  //   longitude: 28.691597768997394,
+  //   latitude: 47.10890434472199,
+  //   city: "",
+  //   state: "",
+  //   image:
+  //     "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
+  // });
 
   const [position, setPosition] = useState({
     longitude: 28.691597768997394,
@@ -53,6 +51,13 @@ const Page: FC = () => {
         mapStyle="mapbox://styles/madzephyr/cm2jenk0k000n01ochr9784jj"
         style={{ width: "100vw", height: "100vh" }}
         testMode
+        onDrag={(e: ViewStateChangeEvent) => {
+          const { viewState } = e;
+          setPosition({
+            latitude: viewState.latitude,
+            longitude: viewState.longitude,
+          });
+        }}
       >
         <GeolocateControl position="top-left" showAccuracyCircle />
         <FullscreenControl position="top-left" />
